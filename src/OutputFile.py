3759b8c6a0f46e97d3104d15377d3e8c25ca6605
Ross Barnowski <rossbar@berkeley.edu>
2013-01-15 16:40:02 -0800

    Finished OutputFileClass constructor. Needs to be tested.

--- src/OutputFile.py
+++ src/OutputFile.py
@@ -3,17 +3,39 @@ import tables
 import os
 
 # User-defined
+from cython64 import getPtrs_cy
 from InputFile import InputFileClass
 
 class OutputFileClass( object ):
   '''Class for creating the output file. Takes an InputFileClass object as 
-     input (this guarantees the event data has been properly processed).'''
-  def __init__( self, fname, inputFile ):
-    # Check if file already exists, don't write over it
-    if os.path.exists( fname ):
+     input (this guarantees the event data has been properly processed). The
+     name of the output file is: "translatedFrom_<original file name>".'''
+  def __init__( self, inputFile ):
+    # Set new file name
+    path = inputFile.hf.filename.split('/')
+    self.origName = path.pop()
+    self.foutname = 'translatedFrom_%s' %(self.origName)
+    path.append( self.foutname )
+    self.foutpath = '/'.join( path )
+    # Make sure file doesn't already exist
+    if os.path.exists( self.foutpath ):
       print 'Warning: file already exists. Exiting...'
       return
-    # Create the file for writing. Use format specified in sandbox.
-    if type( inputFile ) != InputFileClass:
-      print 'Warning: Input file must be of type InputFileClass. Exiting...'
-      return
+    # Get the event pointers from the input file
+    self.ptrs, self.lens = getPtrs_cy.getEventPtrs( inputFile.edata.timestamp,\
+                                                    40, 4, 18 )
+    # Initialize the file
+    self.hf = tables.openFile( self.foutpath, 'w' )
+    self.hf.createGroup( '/', 'SPEIR' )
+    self.hf.createArray( "/SPEIR", "evPtrs", self.ptrs )
+    self.hf.createArray( "/SPEIR", "evEnds", self.lens )
+    # Define the spier data type
+    exprows, siglen = inputFile.hf.root.RawData.shape
+    self.spierout = np.dtype([('Time', '<u8'), ('Energy', '<f4'),\
+                    ('Trig', '<u2'), ('Chan', '<u2'), ('Side', '|u1'),\
+                    ('DetNum', '|u1'), ('Signal', np.uint32, (siglen,)) ])
+    # Create the spier table
+    self.table = self.hf.createTable( '/SPEIR', 'evData', spierout,\
+                 expectedrows=exprows )
+    # Make the input file an attribute
+    self.inputFile = inputFile
